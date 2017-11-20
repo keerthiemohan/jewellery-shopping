be96a562b3d423a1c9d5c06884ae5ae66f5fbf4c
keerthiemohan <keerthiemohan@gmail.com>
2017-11-20 18:14:48 +0530

    My Project Setup is Completed Successfully!

---
+++ jewelleryshopping/src/main/java/frontend/ere/jewelleryshopping/App.java
@@ -0,0 +1,13 @@
+package frontend.ere.jewelleryshopping;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
